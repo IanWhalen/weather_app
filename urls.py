4106412f6ee155e042937d302a4ca79b67de6e28
Ian Whalen <ian.whalen@gmail.com>
2010-11-25 14:51:46 -0500

    App returns weather XML for hard-coded day to browser.

---
+++ urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls.defaults import *
+from weather_app.ndfd.views import *
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    ('^$', homepage_view),
+
+    # Example:
+    # (r'^weather_app/', include('weather_app.foo.urls')),
+
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # to INSTALLED_APPS to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # (r'^admin/', include(admin.site.urls)),
+)
